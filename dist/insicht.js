acfe96eb1cadcd1b6209f4c85013ed0c249a75f2
iamso <so@dev.so>
2019-12-11 14:50:01 +0100

    0.6.0

--- dist/insicht.js
+++ dist/insicht.js
@@ -1,5 +1,5 @@
 /*!
- * insicht - version 0.5.0
+ * insicht - version 0.6.0
  *
  * Made with ❤ by Steve Ottoz so@dev.so
  *
@@ -23,12 +23,6 @@
   Object.defineProperty(exports, "__esModule", {
     value: true
   });
-
-  var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
-    return typeof obj;
-  } : function (obj) {
-    return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
-  };
 
   var _extends = Object.assign || function (target) {
     for (var i = 1; i < arguments.length; i++) {
@@ -143,33 +137,13 @@
     }, {
       key: 'connect',
       value: function connect() {
+        var _this2 = this;
+
         this.disconnect();
-        var _iteratorNormalCompletion = true;
-        var _didIteratorError = false;
-        var _iteratorError = undefined;
-
-        try {
-          for (var _iterator = this._items[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-            var item = _step.value;
-
-            /^f/.test(_typeof(this.options.init)) && this.options.init.apply(this, [item, this]);
-            this.intersection.observe(item);
-          }
-        } catch (err) {
-          _didIteratorError = true;
-          _iteratorError = err;
-        } finally {
-          try {
-            if (!_iteratorNormalCompletion && _iterator.return) {
-              _iterator.return();
-            }
-          } finally {
-            if (_didIteratorError) {
-              throw _iteratorError;
-            }
-          }
-        }
-
+        this._items.forEach(function (item) {
+          _this2.options.init instanceof Function && _this2.options.init.apply(_this2, [item, _this2]);
+          _this2.intersection.observe(item);
+        });
         if (this.options.autoRefresh) {
           this.mutation.observe(this.options.container, {
             subtree: true,
@@ -201,7 +175,7 @@
             stagger = i === 0 ? 0 : stagger + (+item.dataset.stagger || this.options.stagger);
             item.style.transitionDelay = stagger + 'ms';
             item.classList.add(this.options.visibleClass);
-            /^f/.test(_typeof(this.options.done)) && this.options.done.apply(this, [item, this]);
+            this.options.done instanceof Function && this.options.done.apply(this, [item, this]);
             if (this.options.autoRemove) {
               this.remove(item);
             }
@@ -214,61 +188,21 @@
     }, {
       key: 'onMutation',
       value: function onMutation(mutations) {
+        var _this3 = this;
+
         mutations = [].slice.call(mutations, 0);
-        var _iteratorNormalCompletion2 = true;
-        var _didIteratorError2 = false;
-        var _iteratorError2 = undefined;
-
-        try {
-          for (var _iterator2 = mutations[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-            var mutation = _step2.value;
-
-            var update = false;
-            var _iteratorNormalCompletion3 = true;
-            var _didIteratorError3 = false;
-            var _iteratorError3 = undefined;
-
-            try {
-              for (var _iterator3 = [].slice.call(mutation.addedNodes, 0)[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-                var item = _step3.value;
-
-                if (item.nodeType === Node.ELEMENT_NODE && (this.matches(item, this.options.selector) || item.querySelectorAll(this.options.selector).length)) {
-                  update = true;
-                }
-              }
-            } catch (err) {
-              _didIteratorError3 = true;
-              _iteratorError3 = err;
-            } finally {
-              try {
-                if (!_iteratorNormalCompletion3 && _iterator3.return) {
-                  _iterator3.return();
-                }
-              } finally {
-                if (_didIteratorError3) {
-                  throw _iteratorError3;
-                }
-              }
+        mutations.forEach(function (mutation) {
+          var update = false;
+          [].slice.call(mutation.addedNodes, 0).forEach(function (item) {
+            if (item.nodeType === Node.ELEMENT_NODE && (_this3.matches(item, _this3.options.selector) || item.querySelectorAll(_this3.options.selector).length)) {
+              update = true;
             }
+          });
 
-            if (update) {
-              this.refresh();
-            }
+          if (update) {
+            _this3.refresh();
           }
-        } catch (err) {
-          _didIteratorError2 = true;
-          _iteratorError2 = err;
-        } finally {
-          try {
-            if (!_iteratorNormalCompletion2 && _iterator2.return) {
-              _iterator2.return();
-            }
-          } finally {
-            if (_didIteratorError2) {
-              throw _iteratorError2;
-            }
-          }
-        }
+        });
       }
     }, {
       key: 'matches',
@@ -288,31 +222,11 @@
     }, {
       key: 'reset',
       value: function reset() {
-        var _iteratorNormalCompletion4 = true;
-        var _didIteratorError4 = false;
-        var _iteratorError4 = undefined;
+        var _this4 = this;
 
-        try {
-          for (var _iterator4 = this._items[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
-            var item = _step4.value;
-
-            item.classList.remove(this.options.visibleClass);
-          }
-        } catch (err) {
-          _didIteratorError4 = true;
-          _iteratorError4 = err;
-        } finally {
-          try {
-            if (!_iteratorNormalCompletion4 && _iterator4.return) {
-              _iterator4.return();
-            }
-          } finally {
-            if (_didIteratorError4) {
-              throw _iteratorError4;
-            }
-          }
-        }
-
+        this._items.forEach(function (item) {
+          item.classList.remove(_this4.options.visibleClass);
+        });
         return this;
       }
     }, {
--- dist/insicht.next.js
+++ dist/insicht.next.js
@@ -1,5 +1,5 @@
 /*!
- * insicht - version 0.5.0
+ * insicht - version 0.6.0
  *
  * Made with ❤ by Steve Ottoz so@dev.so
  *
@@ -83,10 +83,10 @@ export default class InSicht {
    */
   connect() {
     this.disconnect();
-    for (let item of this._items) {
-      /^f/.test(typeof this.options.init) && this.options.init.apply(this, [item, this]);
+    this._items.forEach(item => {
+      this.options.init instanceof Function && this.options.init.apply(this, [item, this]);
       this.intersection.observe(item);
-    }
+    });
     if (this.options.autoRefresh) {
       this.mutation.observe(this.options.container, {
         subtree: true,
@@ -124,7 +124,7 @@ export default class InSicht {
         stagger = i === 0 ? 0 : stagger + (+item.dataset.stagger || this.options.stagger);
         item.style.transitionDelay = `${stagger}ms`;
         item.classList.add(this.options.visibleClass);
-        /^f/.test(typeof this.options.done) && this.options.done.apply(this, [item, this]);
+        this.options.done instanceof Function && this.options.done.apply(this, [item, this]);
         if (this.options.autoRemove) {
           this.remove(item);
         }
@@ -141,17 +141,18 @@ export default class InSicht {
    */
   onMutation(mutations) {
     mutations = [].slice.call(mutations, 0);
-    for (let mutation of mutations) {
+    mutations.forEach(mutation => {
       let update = false;
-      for (let item of [].slice.call(mutation.addedNodes, 0)) {
+      [].slice.call(mutation.addedNodes, 0).forEach(item => {
         if (item.nodeType === Node.ELEMENT_NODE && (this.matches(item, this.options.selector) || item.querySelectorAll(this.options.selector).length)) {
           update = true;
         }
-      }
+      });
+
       if (update) {
         this.refresh();
       }
-    }
+    });
   }
 
   /**
@@ -202,9 +203,9 @@ export default class InSicht {
    * @return {Object} - InSicht instance
    */
   reset() {
-    for (let item of this._items) {
+    this._items.forEach(item => {
       item.classList.remove(this.options.visibleClass);
-    }
+    });
     return this;
   }
 
